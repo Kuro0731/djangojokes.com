f3527ee23b2a675b0583d779e3b37f6096c71687
Kuro0731 <woodcoty99@gmail.com>
2024-08-08 04:25:14 -0500

    Restricting Joke Creating and Updating (36th) Commit

--- jokes/views.py
+++ jokes/views.py
@@ -1,5 +1,5 @@
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.urls import reverse_lazy
-
 from django.views.generic import (
     CreateView, DeleteView, DetailView, ListView, UpdateView
 )
@@ -7,7 +7,7 @@ from django.views.generic import (
 from .models import Joke
 from .forms import JokeForm
 
-class JokeCreateView(CreateView):
+class JokeCreateView(LoginRequiredMixin, CreateView):
     model = Joke
     form_class = JokeForm
 
@@ -16,9 +16,13 @@ class JokeCreateView(CreateView):
         return super().form_valid(form)
 
 
-class JokeDeleteView(DeleteView):
+class JokeDeleteView(UserPassesTestMixin, DeleteView):
     model = Joke
     success_url = reverse_lazy('jokes:list')
+
+    def test_func(self):
+        obj = self.get_object()
+        return self.request.user == obj.user
 
 
 class JokeDetailView(DetailView):
@@ -29,6 +33,10 @@ class JokeListView(ListView):
     model = Joke
 
 
-class JokeUpdateView(UpdateView):
+class JokeUpdateView(UserPassesTestMixin, UpdateView):
     model = Joke
     form_class = JokeForm
+
+    def test_func(self):
+        obj = self.get_object()
+        return self.request.user == obj.user
